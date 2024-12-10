1a1998604602fe0229fbcf4404600cfbbd6c84e5
chahalamol <51814586+chahalamol@users.noreply.github.com>
2024-12-09 20:01:54 -0500

    small updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,13 +11,13 @@ export default function Home() {
     <div className="min-h-screen bg-[#FDF5E6] bg-[length:6px_6px] bg-[linear-gradient(to_right,rgba(0,0,0,0.03)_1px,transparent_1px),linear-gradient(to_bottom,rgba(0,0,0,0.03)_1px,transparent_1px)]">
       <main className="flex flex-col items-center justify-center p-8">
         <div className="max-w-2xl mx-auto text-center mt-12">
-          <h1 className="text-3xl font-serif">aromatic</h1>
-          <p className="text-xs font-regular text-gray-600">a haphazard alchemy</p>
+          <h1 className="text-3xl font-semibold font-serif">aromatic</h1>
+          <p className="text-xs font-medium font-sans text-gray-600">a haphazard alchemy</p>
           
           <div className="mt-12 flex flex-col items-center">
            <div className="flex items-center gap-8 mb-8 w-full">
               <div className="text-sm text-left text-gray-600 grow">
-                <p className="mb-2 font-bold">v1.0 // 30 ml</p>
+                <p className="mb-2 font-bold font-sans">v0.1 // 30 ml</p>
                 <p className="text-xs font-sans">EARTH  MUSK  PATCHOULI  <br />BIRCHWOOD  VIOLET  <br />SANDALWOOD  OAKMOSS</p>
               </div>
               <Image
@@ -72,7 +72,7 @@ export default function Home() {
             </div>
 
             <h2 className="text-md font-sans font-bold mb-4 self-start">Vancouver(21)</h2>
-            <p className="text-gray-700 max-w-lg leading-relaxed text-left">
+            <p className="text-gray-700 font-sans max-w-lg leading-relaxed text-left">
               A breath of damp earth and rain-kissed leaves, evoking the
               serene stillness of a lush forest after spring rain. It captures the
               delicate interplay of birchwood and violet, enriched by the
